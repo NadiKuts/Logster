8b34e941ce408df0949aaa5cb098a9f26cae2fa2
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-06-11 12:54:34 +0200

    Delete unnecessary rows

--- app/services/service.js
+++ app/services/service.js
@@ -8,23 +8,6 @@ controllers.factory('API', ['$http', '$timeout', 'Server', function ($http, $tim
     API.getEvents = function () {
         return $http.get(Server + '/api/events')
     };
-    
-    
-    /*API.getProfile = function (id) {
-        return $http.get(Server + '/' + id);
-    };
-
-    API.insertProfile = function (report) {
-        return $http.post(Server, report);
-    };
-
-    API.updateProfile = function (id, report) {
-        return $http.put(Server + '/' + id, report)
-    };
-
-    API.deleteProfile = function (id) {
-        return $http.delete(Server + '/' + id);
-    };*/
 
     return API;
 }])
