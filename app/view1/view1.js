b5d258f5bd02eb188d1ad957eff57308dd6382e1
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-06-11 11:29:14 +0200

    Add icons

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,13 +11,20 @@ controllers.config(['$routeProvider', function($routeProvider) {
 }]);
 
 controllers.controller('View1Ctrl', ['$scope', '$interval', 'API', function ($scope, $interval, API) {
+  $scope.count = 0;
    var refreshData = function() { 
      API.getEvents().then(function (data) {
+       /*$scope.events = [];
+       for (var j=0; j<data.data.data.length; j++) {
+         $scope.events.push(data.data.data[j]);
+         $scope.events[j].class = 'abc';
+       }*/
        $scope.events = data.data.data;
        console.log($scope.events);
        $scope.labels = [];
        $scope.data = [[]];
        for (var i = 0; i < $scope.events.length; i++) {
+         $scope.events[i].class = 'abx';
          var event = $scope.events[i];
          if ($scope.isSL(event)) {
            $scope.data[0].push(parseInt(event.value, 10));
@@ -32,12 +39,16 @@ controllers.controller('View1Ctrl', ['$scope', '$interval', 'API', function ($sc
   var promise = $interval(refreshData, 2000);
   console.log($scope.events);
   
-
-
+  /*if ($scope.count != $scope.events.length) {
+    $scope.events[0].class = 'NEW';
+    $scope.count = $scope.events.length;
+  }*/
   
   $scope.isSL = function(event) {
     return event.type == 'SL'
   }
+  
+  
   
   $scope.toTime = function(event) {
     var time = event.date.substring(0, 10) + ' ' + event.date.substring(11, 16);
